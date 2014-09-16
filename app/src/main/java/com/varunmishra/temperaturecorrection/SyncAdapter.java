3dfbd7d956ea9a6a60666572d05860ee41451fb4
varun <varun@varunmishra.com>
2014-09-17 02:50:31 +0530

    fixing.

---
+++ app/src/main/java/com/varunmishra/temperaturecorrection/SyncAdapter.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.varunmishra.temperaturecorrection;
+
+import android.accounts.Account;
+import android.content.AbstractThreadedSyncAdapter;
+import android.content.ContentProviderClient;
+import android.content.ContentProviderOperation;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.OperationApplicationException;
+import android.content.SyncResult;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.util.Log;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Define a sync adapter for the app.
+ *
+ * <p>This class is instantiated in {@link SyncService}, which also binds SyncAdapter to the system.
+ * SyncAdapter should only be initialized in SyncService, never anywhere else.
+ *
+ * <p>The system calls onPerformSync() via an RPC call through the IBinder object supplied by
+ * SyncService.
+ */
+public class SyncAdapter extends AbstractThreadedSyncAdapter {
+    ContentResolver mContentResolver;
+
+    /**
+     * Set up the sync adapter
+     */
+    public SyncAdapter(Context context, boolean autoInitialize) {
+        super(context, autoInitialize);
+        /*
+         * If your app uses a content resolver, get an instance of it
+         * from the incoming Context
+         */
+        mContentResolver = context.getContentResolver();
+    }
+
+
+    /**
+     * Set up the sync adapter. This form of the
+     * constructor maintains compatibility with Android 3.0
+     * and later platform versions
+     */
+    public SyncAdapter(
+            Context context,
+            boolean autoInitialize,
+            boolean allowParallelSyncs) {
+        super(context, autoInitialize, allowParallelSyncs);
+        /*
+         * If your app uses a content resolver, get an instance of it
+         * from the incoming Context
+         */
+        mContentResolver = context.getContentResolver();
+
+    }
+
+    @Override
+    public void onPerformSync(Account account, Bundle bundle, String s, ContentProviderClient contentProviderClient, SyncResult syncResult) {
+       Log.d("UPUP","Reached here");
+        try {
+            Cursor cursor = contentProviderClient.query(Uri.parse("content://com.varunmishra.temperaturecorrection.provider/datacollected"),null, null, null, null);
+            cursor.moveToFirst();
+            StringBuilder res=new StringBuilder();
+                while (!cursor.isAfterLast()) {
+                    String timestamp = cursor.getString(cursor.getColumnIndex("timestamp"));
+                    String value = cursor.getString(cursor.getColumnIndex("value"));
+
+                    HttpClient httpclient = new DefaultHttpClient();
+                    HttpPost httppost = new HttpPost("http://varunmishra.com/android-test/upload.php");
+                    try {
+                        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+                        nameValuePairs.add(new BasicNameValuePair("id", timestamp));
+                        nameValuePairs.add(new BasicNameValuePair("value",value));
+                        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                        httpclient.execute(httppost);
+                    } catch (ClientProtocolException e) {
+                        // TODO Auto-generated catch block
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                    }
+                    contentProviderClient.delete(Uri.parse("content://com.varunmishra.temperaturecorrection.provider/datacollected"),"timestamp=?", new String[]{timestamp});
+                   cursor = contentProviderClient.query(Uri.parse("content://com.varunmishra.temperaturecorrection.provider/datacollected"),null, null, null, null);
+                    cursor.moveToFirst();
+                }
+            } catch (RemoteException e) {
+            syncResult.hasHardError();
+            e.printStackTrace();
+        }
+
+
+    }
+}
